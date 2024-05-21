a5b61643aa81a24921be532374d45dc29f2a1620
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-21 15:44:48 +0530

    Client

--- py_client/create.py
+++ py_client/create.py
@@ -1,8 +1,10 @@
 import requests
-
+headers = {
+    "Authorization": "Bearer f38f8b17b6fbdb0e1f1add312ad1254c9441a75e"
+}
 endpoint = "http://localhost:8000/api/products/"
 data = {
-    "title":"Field Done",
+    "title":"Field Done wonderful",
 }
-get_response = requests.post(endpoint,json=data)
+get_response = requests.post(endpoint,json=data,headers=headers)
 print(get_response.json())
