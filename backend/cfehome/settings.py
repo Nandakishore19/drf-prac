ea9b4c1b5732b90d7849e171e831c3dbed9a3e79
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-21 15:42:59 +0530

    default

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'products',
+    'search',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework.authentication.SessionAuthentication",
+        "api.authentication.TokenAuthentication"
+    ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+    "rest_framework.permissions.IsAuthenticatedOrReadOnly",   
+      ],
+      'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination.LimitOffsetPagination",
+      "PAGE_SIZE":10
+}
